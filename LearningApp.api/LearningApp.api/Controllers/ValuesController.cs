ada5a0c7c05e5d16f5737d4fba9b73b23c5ca9d5
PA_RSidharthan <PA_RSidharthan@nslijhs.net>
2019-04-22 13:29:39 -0400

    Initial Commit

---
+++ LearningApp.api/LearningApp.api/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using LearningApp.api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningApp.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContxt _contxt;
+
+        public ValuesController(DataContxt context )
+        {
+            _contxt = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _contxt.Values.ToListAsync();
+            return  Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await  _contxt.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LearningApp.api/LearningApp.api/Data/DataContxt.cs
@@ -0,0 +1,16 @@
+﻿using LearningApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningApp.api.Data
+{
+    public class DataContxt:DbContext
+    {
+        public DataContxt(DbContextOptions<DataContxt> options) : base(options) { }
+        
+        public DbSet<Value> Values { get; set; }        
+    }
+}
